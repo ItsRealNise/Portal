4a9a2b8fd8db67a9292d7e37367847fb858cafb4
ItsRealNise <90197402+ItsRealNise@users.noreply.github.com>
2021-09-22 16:29:16 +0700

    Create PlayerEnterPortalEvent.php

---
+++ src/ItsRealNise/Portal/event/player/PlayerEnterPortalEvent.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+namespace ItsRealNise\Portal\event\player;
+
+use pocketmine\event\Cancellable;
+use pocketmine\Player;
+use ItsRealNise\Portal\block\multiblock\PortalMultiBlock;
+use ItsRealNise\Portal\event\DimensionPortalsEvent;
+
+/**
+ * Class PlayerEnterPortalEvent
+ * @package Xenophilicy\TableSpoon\event\player
+ */
+class PlayerEnterPortalEvent extends DimensionPortalsEvent implements Cancellable {
+    
+    /** @var Player */
+    private $player;
+    
+    /** @var PortalMultiBlock */
+    private $block;
+    
+    /** @var int */
+    private $teleportDuration;
+    
+    /**
+     * PlayerEnterPortalEvent constructor.
+     * @param Player $player
+     * @param PortalMultiBlock $block
+     * @param int $teleportDuration
+     */
+    public function __construct(Player $player, PortalMultiBlock $block, int $teleportDuration){
+        $this->player = $player;
+        $this->block = $block;
+        $this->teleportDuration = $teleportDuration;
+    }
+    
+    public function getPlayer(): Player{
+        return $this->player;
+    }
+    
+    public function getBlock(): PortalMultiBlock{
+        return $this->block;
+    }
+    
+    public function getTeleportDuration(): int{
+        return $this->teleportDuration;
+    }
+    
+    public function setTeleportDuration(int $teleportDuration): void{
+        $this->teleportDuration = $teleportDuration;
+    }
+}
