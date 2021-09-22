bd6e3920749b1fd88adae67764146c2f772c8ab0
ItsRealNise <90197402+ItsRealNise@users.noreply.github.com>
2021-09-22 16:27:56 +0700

    Create DimensionPortalsEvent.php

---
+++ src/ItsRealNise/Portal/event/DimensionPortalsEvent.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace ItsRealNise\Portal\event;
+
+use pocketmine\event\Event;
+
+/**
+ * Class DimensionPortalsEvent
+ * @package ItsRealNise\Portal\event
+ */
+abstract class DimensionPortalsEvent extends Event {
+}
